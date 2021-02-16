d65b5de1be76aa81f1ea97e2f227a15ba5fc6082
Matheus <matheusssleite@outlook.com>
2021-02-16 19:29:45 -0300

    FINISHIN IN PAGE TRABALHECONOSCO.

--- AppBancoDominio/TrabalheConosco.cs
+++ AppBancoDominio/TrabalheConosco.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,15 @@ namespace AppBancoDominio
         public int cd_usu { get; set; }
 
         [DisplayName("Nome:")]
+        [Required(ErrorMessage = "Obrigatório digitar o Nome!")]
         public string nm_usu { get; set; }
 
         [DisplayName("Telefone:")]
+        [Required(ErrorMessage = "Obrigatório digitar um Número de Telefone!")]
         public string tel_usu { get; set; }
 
         [DisplayName("E-mail:")]
+        [Required(ErrorMessage = "Obrigatório digitar seu e-mail!")]
         public string email_usu { get; set; }
     }
 }
