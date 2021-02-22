620c70ea2913e042a15136294fe43342f0d69966
Matheus <matheusssleite@outlook.com>
2021-02-22 17:02:01 -0300

    CREATE CLASS FUNCIONARIO IN APPBANCODOMINIO

---
+++ AppBancoDominio/Funcionario/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppBancoDominio.Funcionario
+{
+    public class Funcionario
+    {
+        [DisplayName("ID do Funcionário:")]
+        public int id_func { get; set; }
+
+        [DisplayName("Nome do Funcionário:")]
+        [Required(ErrorMessage = "Obrigatório digitar o Nome do Funcionário!")]
+        public string nm_func { get; set; }
+
+        [DisplayName("Cargo do Funcionário:")]
+        [Required(ErrorMessage = "Obrigatório informar o Cargo do Funcionário!")]
+        public string cg_func { get; set; }
+
+        [DisplayName("E-mail:")]
+        [Required(ErrorMessage = "Obrigatório informar algum e-mail!")]
+        public string email_func { get; set; }
+
+        [DisplayName("CPF do Funcionário:")]
+        [StringLength(14, MinimumLength = 14, ErrorMessage = "Esse campo deve ter 11 caracteres!")]
+        public string cpf_func { get; set; }
+        
+        [DisplayName("Endereço do Funcionário:")]
+        [Required(ErrorMessage = "Obrigatório informar algum endereço do Funcionário!")]
+        public string end_func { get; set; }
+        
+        [DisplayName("Telefone do Funcionário:")]
+        [Required(ErrorMessage = "Obrigatório informar um número de Telefone!")]
+        public string tel_func { get; set; }
+    }
+}
