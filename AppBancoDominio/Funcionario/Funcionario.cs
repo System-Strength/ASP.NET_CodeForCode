bfb390d6a7c64f3e7d6403dd64e96bee192f0e33
Matheus <matheusssleite@outlook.com>
2021-02-23 18:21:58 -0300

    RENAME DISPLAYNAME IN CLASS FUNCIONARIO APPBANCODOMINIO

--- AppBancoDominio/Funcionario/Funcionario.cs
+++ AppBancoDominio/Funcionario/Funcionario.cs
@@ -5,31 +5,31 @@ namespace AppBancoDominio
 {
     public class Funcionario
     {
-        [DisplayName("ID do Funcionário:")]
+        [DisplayName("ID")]
         public int id_func { get; set; }
 
-        [DisplayName("Nome do Funcionário:")]
+        [DisplayName("Nome")]
         [Required(ErrorMessage = "Obrigatório digitar o Nome do Funcionário!")]
         public string nm_func { get; set; }
 
-        [DisplayName("Cargo do Funcionário:")]
+        [DisplayName("Cargo")]
         [Required(ErrorMessage = "Obrigatório informar o Cargo do Funcionário!")]
         public string cg_func { get; set; }
 
-        [DisplayName("E-mail:")]
+        [DisplayName("Email")]
         [Required(ErrorMessage = "Obrigatório informar algum e-mail!")]
         public string email_func { get; set; }
 
-        [DisplayName("CPF do Funcionário:")]
+        [DisplayName("CPF")]
         [StringLength(14, MinimumLength = 14, ErrorMessage = "Esse campo deve ter 11 caracteres!")]
         [Required(ErrorMessage = "Obrigatório digitar CPF do Funcionário!")]
         public string cpf_func { get; set; }
         
-        [DisplayName("Endereço do Funcionário:")]
+        [DisplayName("Endereço")]
         [Required(ErrorMessage = "Obrigatório informar algum endereço do Funcionário!")]
         public string end_func { get; set; }
         
-        [DisplayName("Telefone do Funcionário:")]
+        [DisplayName("Telefone")]
         [Required(ErrorMessage = "Obrigatório informar um número de Telefone!")]
         public string tel_func { get; set; }
     }
